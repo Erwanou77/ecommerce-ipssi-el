88a3c8402346f209dafcc76651e149bede82a6d3
Erwanou77 <erwan.launay77@gmail.com>
2022-12-16 00:35:25 +0100

    📦 NEW: Add Cart

--- src/Controller/ContentController.php
+++ src/Controller/ContentController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Cart;
+use App\Entity\CartsProducts;
 use App\Entity\Product;
+use App\Entity\User;
+use App\Form\AddCartType;
+use App\Repository\CartsProductsRepository;
 use App\Repository\ProductRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -34,11 +39,41 @@ class ContentController extends AbstractController
         ]);
     }
 
-    #[Route('/product/{id}', name: 'app_product_read', methods: ['GET'])]
-    public function show(Product $product): Response
+    #[Route('/product/{id}', name: 'app_product_read', methods: ['GET', 'POST'])]
+    public function show(Request $request, Product $product, CartsProductsRepository $cpRepository): Response
     {
+        $status = NULL;
+        $user = $this->getUser();
+        $cart = $user->getCart();
+        $cp = $cart->getCartsProducts()->toArray();
+
+        $cartProduct = new CartsProducts();
+        $cartProduct->setCart($cart);
+        $form = $this->createForm(AddCartType::class, $cartProduct);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cartProduct->setProduct($product);
+            $quantity = $form->getData()->getQuantity();
+            $cartProduct->setQuantity($quantity);
+            if (isset($cp)) {
+                foreach ($cp as $cpProduct) {
+                    if ($cpProduct->getProduct() == $cartProduct->getProduct() && $cpProduct->getCart() == $cartProduct->getCart()) {
+                        $quantity = $quantity + $cpProduct->getQuantity();
+                        $cpProduct->setQuantity($quantity);
+                        $cpRepository->save($cpProduct, true);
+                        $status = true;
+                    }
+                }
+            }
+            if (empty($status)) {
+                $cpRepository->save($cartProduct, true);
+            }
+            return $this->redirectToRoute('app_cart');
+        }
         return $this->render('content/product/read.html.twig', [
             'product' => $product,
+            'form' => $form->createView()
         ]);
     }
 }
